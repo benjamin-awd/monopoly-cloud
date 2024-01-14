f6235a306093ca439d1888acc94cf181c50c4946
Benjamin Dornel <benjamindornel@gmail.com>
2024-01-14 12:28:45 +0800

    refactor(gmail): improve nested search logic

--- tests/unit/test_get_attachment.py
+++ tests/unit/test_get_attachment.py
@@ -1,17 +1,20 @@
+import pytest
+
 from monocloud.gmail import Message
+from monocloud.gmail.exceptions import AttachmentNotFoundError, MultipleAttachmentsError
 
 
-def test_message_with_parts(message: Message):
+def test_message_with_part_attachment(message: Message):
     data = {
         "payload": {
+            "filename": "",
             "parts": [
                 {
                     "partId": "1",
                     "filename": "file1.txt",
                     "body": {"attachmentId": "123"},
-                },
-                {"partId": "2", "body": {"attachmentId": "456"}},
-            ]
+                }
+            ],
         }
     }
     message.payload = data["payload"]
@@ -20,7 +23,7 @@ def test_message_with_parts(message: Message):
     assert attachment_id == "123"
 
 
-def test_message_with_nested_parts(message: Message):
+def test_message_with_multiple_attachment(message: Message):
     data = {
         "payload": {
             "parts": [
@@ -44,14 +47,22 @@ def test_message_with_nested_parts(message: Message):
         }
     }
     message.payload = data["payload"]
-    attachment_id, filename = message.get_attachment_metadata()
-    assert filename == "file1.txt"
-    assert attachment_id == "123"
+
+    with pytest.raises(MultipleAttachmentsError):
+        message.get_attachment_metadata()
 
 
-def test_message_with_no_parts(message: Message):
+def test_message_with_body_attachment(message: Message):
     data = {"payload": {"filename": "file1.txt", "body": {"attachmentId": "123"}}}
     message.payload = data["payload"]
     attachment_id, filename = message.get_attachment_metadata()
     assert filename == "file1.txt"
     assert attachment_id == "123"
+
+
+def test_message_with_no_attachment(message: Message):
+    data = {"payload": {"headers": {"foo": "bar"}}}
+    message.payload = data["payload"]
+
+    with pytest.raises(AttachmentNotFoundError):
+        message.get_attachment_metadata()
